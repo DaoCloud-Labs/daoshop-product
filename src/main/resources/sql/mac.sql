3282b7dd88a065452a3f7ad3df1fcd27d88951c0
hao <dao.cheng@daocloud.io>
2019-04-09 10:54:01 +0800

    add mac.sql

---
+++ src/main/resources/sql/mac.sql
@@ -0,0 +1,50 @@
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('Polo','Polo','77900','1','1','9999',
+'https://brand.svw-volkswagen.com/images/newPolo.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('Polo GTI','Polo GTI','143900','4','123','132',
+'https://brand.svw-volkswagen.com/images/poloGti.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('Cross Polo','Cross Polo','114900','32','31','3112',
+'https://brand.svw-volkswagen.com/images/crossPolo.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('全新一代朗逸两厢','全新一代朗逸两厢','130900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/granLavida.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('桑塔纳·浩纳','桑塔纳·浩纳','92900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/granSantana.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('全新一代朗逸','全新一代朗逸','112900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/lavida_plus.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('全新凌渡','全新凌渡','142900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/lamando.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('朗逸','朗逸','107900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/newLavida.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('新桑塔纳','新桑塔纳','86900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/NewSantana.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('全新一代帕萨特','全新一代帕萨特','184900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/passat.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('帕萨特插电式混合动力版','帕萨特插电式混合动力版','243900','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/passat-phev.png');
+
+insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
+values ('辉昂','辉昂','340000','31','31312','313123',
+'https://brand.svw-volkswagen.com/images/PHIDEON.png');
+
+
+
