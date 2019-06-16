f47b41e9ba71078109c5d5d06aa8e348a69d21a8
hao <dao.cheng@daocloud.io>
2019-06-16 15:30:57 +0800

    git add image

--- src/main/resources/sql/data-mysql.sql
+++ src/main/resources/sql/data-mysql.sql
@@ -1,50 +1,47 @@
-insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
-values ('DaoCloud Micro Service Platform','DaoCloud Micro Service Platform','9999999','1','1','9999',
-'https://qiniu-download-public.daocloud.io/DaoCloud_MicroServices_Platform/dmp_logo.svg');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('Beats EP头戴式耳机','Beats EP头戴式耳机','558','4','123','132',
-'http://img11.360buyimg.com/n1/s528x528_jfs/t3109/194/2435573156/46587/e0e867ac/57e10978N87220944.jpg!q70.jpg');
+'/demo/product/v1/1.jpg');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('雀巢（Nestle）高钙成人奶粉','','5218','32','31','3112',
-'http://m.360buyimg.com/babel/jfs/t5197/28/400249159/97561/304ce550/58ff0dbeN88884779.jpg!q50.jpg.webp');
+'/demo/product/v1/2.jpg.webp');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('煎炒烹炸一锅多用','煎炒烹炸一锅多用','228','31','31312','313123',
-'http://gw.alicdn.com/tps/TB19OfQRXXXXXbmXXXXL6TaGpXX_760x760q90s150.jpg_.webp');
+'/demo/product/v1/3.jpg_.webp');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('ANNE KLEIN 潮流经典美式轻奢','ANNE KLEIN 潮流经典美式轻奢','228','31','31312','313123',
-'http://gw.alicdn.com/tps/TB1l5psQVXXXXcXaXXXL6TaGpXX_760x760q90s150.jpg_.webp');
+'/demo/product/v1/4.jpg_.webp');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('乐高EV3机器人积木玩具','乐高EV3机器人积木玩具','228','31','31312','313123',
-'https://m.360buyimg.com/mobilecms/s357x357_jfs/t6490/168/1052550216/653858/9eef28d1/594922a8Nc3afa743.jpg!q50.jpg');
+'/demo/product/v1/5.jpg');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('全球购 路易威登（Louis Vuitton）新款女士LV印花手袋 M41112','全球购 路易威登（Louis Vuitton）新款女士LV印花手袋 M41112','228','31','31312','313123',
-'https://m.360buyimg.com/n1/s220x220_jfs/t1429/17/1007119837/464370/310392f4/55b5e5bfN75daf703.png!q70.jpg');
+'/demo/product/v1/6.png');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('''Kindle Paperwhite3 黑色经典版电纸书','''Kindle Paperwhite3 黑色经典版电纸书','228','31','31312','313123',
-'http://gw.alicdn.com/tps/TB19OfQRXXXXXbmXXXXL6TaGpXX_760x760q90s150.jpg_.webp');
+'/demo/product/v1/7.jpg_.webp');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('DELSEY 男士双肩背包','DELSEY 男士双肩背包','228','31','31312','313123',
-'http://gw.alicdn.com/tps/LB1HL0mQVXXXXbzXVXXXXXXXXXX.png');
+'/demo/product/v1/8.png');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('荷兰 天赋力 Herobaby 婴儿配方奶粉 4段 1岁以上700g','荷兰 天赋力 Herobaby 婴儿配方奶粉 4段 1岁以上700g','228','31','31312','313123',
-'http://m.360buyimg.com/babel/s330x330_jfs/t4597/175/4364374663/125149/4fbbaf21/590d4f5aN0467dc26.jpg!q50.jpg.webp');
+'/demo/product/v1/9.jpg.webp');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('【全球购】越南acecook河粉牛肉河粉特产 速食即食方便面粉丝 牛肉河粉米粉65克*5袋','【全球购】越南acecook河粉牛肉河粉特产 速食即食方便面粉丝 牛肉河粉米粉65克*5袋','228','31','31312','313123',
-'https://m.360buyimg.com/mobilecms/s357x357_jfs/t3169/228/5426689121/95568/d463e211/586dbf56N37fcd503.jpg!q50.jpg');
+'/demo/product/v1/10.jpg');
 
 insert into t_product (c_name,c_desc,price,c_type,stock,sales,image)
 values ('正品FENDI/芬迪女包钱包女长款 百搭真皮钱夹 女士小怪兽手拿包','正品FENDI/芬迪女包钱包女长款 百搭真皮钱夹 女士小怪兽手拿包','228','31','31312','313123',
-'http://img.alicdn.com/imgextra/i3/TB16avCQXXXXXcsXpXXXXXXXXXX_!!0-item_pic.jpg_400x400q60s30.jpg_.webp');
+'/demo/product/v1/11.jpg_.webp');
 
 
 
