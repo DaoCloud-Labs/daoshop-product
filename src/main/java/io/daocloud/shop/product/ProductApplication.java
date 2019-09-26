e72de8fb79efdbfa350e34d3fd19d5951d118de6
Jared.Tan <jian.tan@daocloud.io>
2019-09-26 16:24:32 +0800

    add dc

--- src/main/java/io/daocloud/shop/product/ProductApplication.java
+++ src/main/java/io/daocloud/shop/product/ProductApplication.java
@@ -1,15 +1,16 @@
 package io.daocloud.shop.product;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import io.daocloud.annotation.EnableDaocloudDiscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDaocloudDiscovery
 @EnableSwagger2Doc
 public class ProductApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
